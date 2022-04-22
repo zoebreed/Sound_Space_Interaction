b72bbbd194a68d1eed03c9ae7c964073b7673be1
Joyce den Hertog <joycedenhertog@live.nl>
2022-04-22 11:28:50 +0200

    add python stuff

---
+++ eind_SSI/calc_values.py
@@ -0,0 +1,64 @@
+import math
+
+# def calc_angle(pos1, pos2, pos3):
+# 	''' calculates the angle between 2 lines which are defined by 1 points. 
+# 	Inspired by the following tutorial: https://www.youtube.com/watch?v=cIyziswpu_w ''' 
+
+# 	# first line
+# 	dy1 = pos2[2] - pos1[2]
+# 	dx1 = pos2[1] - pos1[1]
+# 	alpha = math.atan2(dy1, dx1)
+
+# 	# second line 
+# 	dy2 = pos3[2] - pos2[2]
+# 	dx2 =  pos3[1] - pos2[1]
+# 	beta = math.atan2(dy2, dx2)
+
+# 	# angle difference
+# 	if alpha - beta <= 90:
+# 		phi = alpha - beta
+# 	else:
+# 		phi = 180 - alpha - beta
+
+# 	return phi 
+
+
+def calc_body_area(x11, x12, x23, x24, y12, y24, frame_shape):
+	length_shoulders = abs(x11 - x12) / (0.1 * frame_shape[0])
+	length_hips = abs(x23 - x24) / (0.1 * frame_shape[0])
+	height_body = abs(y12 - y24) / (0.1 * frame_shape[1])
+
+	# area of trapezium, which is the body
+	body_area = (0.5 * (length_shoulders + length_hips)) * height_body
+
+	return int(body_area)
+
+
+def calc_useful_pose_values(detector, img, lmlst, frame_shape):
+	''' calculates useful values from the list of landmarks. 
+	L = left, R = right, U = upper, L = lower '''
+
+	# angles from arms 
+	angle_arm_UL = int(detector.findAngle(img, 11, 12, 14, draw=False))
+	angle_arm_LL = int(detector.findAngle(img, 12, 14, 16, draw=False))
+	angle_arm_UR = int(detector.findAngle(img, 12, 11, 13, draw=False))
+	angle_arm_LR = int(detector.findAngle(img, 11, 13, 15, draw=False))
+
+	# angles from legs 
+	angle_leg_UL = int(detector.findAngle(img, 23, 24, 26, draw=False))
+	angle_leg_LL = int(detector.findAngle(img, 24, 26, 28, draw=False))
+	angle_leg_UR = int(detector.findAngle(img, 24, 23, 25, draw=False))
+	angle_leg_LR = int(detector.findAngle(img, 23, 25, 27, draw=False))
+
+	# area of the body trapezium
+	body_area = calc_body_area(lmlst[11][1], lmlst[12][1], lmlst[23][1],
+							lmlst[24][1], lmlst[12][2], lmlst[24][2], 
+							frame_shape)
+
+	noseX = int(lmlst[0][1])
+	noseY = int(lmlst[0][2])
+
+	useful_values = [noseX, noseY, body_area, angle_arm_UL, angle_arm_LL, angle_arm_UR, 
+						angle_arm_LR, angle_leg_UL, angle_leg_LL, angle_leg_UR, angle_leg_LR]
+
+	return useful_values
---
+++ eind_SSI/send-poses.py
@@ -0,0 +1,29 @@
+import cv2
+import time
+import poseModule as pm
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
+from calc_values import calc_useful_pose_values
+
+ip = "127.0.0.1"
+port = 3333
+client = udp_client.SimpleUDPClient(ip, port)
+ 
+cap = cv2.VideoCapture(0)
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    frame_shape = (cap.get(3), cap.get(4))
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) !=0:
+        # for i, landmark in enumerate(lmList):
+        #     client.send_message(f"/pose{i}/{landmark[1]}/{landmark[2]}", 100)
+        useful_values = calc_useful_pose_values(detector, img, lmList, frame_shape)
+        for i, value in enumerate(useful_values):
+            client.send_message(f"/{i}", value)
+ 
+    cv2.imshow('MediaPipe Pose', cv2.flip(img, 1))
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
